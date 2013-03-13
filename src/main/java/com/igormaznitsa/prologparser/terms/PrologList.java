8fe286d55bc1929c00efda6356d8643be20a3f73
rrg4400 <rrg4400@gmail.com>
2013-03-13 23:25:00 +0200

    changed year in the copyright info

--- src/main/java/com/igormaznitsa/prologparser/terms/PrologList.java
+++ src/main/java/com/igormaznitsa/prologparser/terms/PrologList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2012 Igor Maznitsa (http://www.igormaznitsa.com)
+ * Copyright 2011-2013 Igor Maznitsa (http://www.igormaznitsa.com)
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of version 3 of the GNU Lesser General Public
